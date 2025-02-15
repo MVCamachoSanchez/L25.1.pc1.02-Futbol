94252c07db748c646c8694b38d6063ff20dfe055
MVCamachoSanchez <mariavalecamacho1105@gmail.com>
2025-02-15 16:01:59 -0400

    new

--- JS/principal.js
+++ JS/principal.js
@@ -1,3 +1,9 @@
+/** Conociendo los resultados de varios juegos de tu equipo de fútbol, donde 1 es ganar y 0 es
+perder, hacer un programa que indique el porcentaje de juegos que ganaste.
+Ej. Resultados de 6 juegos: 1, 1, 0, 1, 0, 1, 1
+la salida requerida presenta el siguiente formato:
+Ganaste el 66.67% de los juegos */
+
 import Cl_Futbol from "./Cl_Futbol.js";
 import Cl_Juego from "./Cl_Juego.js";
 
